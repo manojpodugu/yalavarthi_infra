07d4b82f71831a8f735e14fc9f1e059fa29e69c9
manojpodugu <psmk126@gmail.com>
2025-10-18 20:15:08 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 # s3.tf
 
 resource "aws_s3_bucket" "backups" {
-  bucket = my-backup-bucket-${var.project_name}-${var.environment}-${random_id.bucket_suffix.hex}"
+  bucket = "my-backup-bucket-${var.project_name}-${var.environment}-${random_id.bucket_suffix.hex}"
   tags = {
     Name        = "backup-bucket"
     Environment = "prod"
