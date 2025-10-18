55e51c8536d517eebdc6ced6395013abacb23aca
manojpodugu <psmk126@gmail.com>
2025-10-18 12:35:45 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,8 +4,8 @@ resource "aws_instance" "app" {
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   key_name               = length(trim(var.key_name)) > 0 ? var.key_name : null
-  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
-  associate_public_ip_address = true
+  #iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+  #associate_public_ip_address = true
 
 
   tags = {
