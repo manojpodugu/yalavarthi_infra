6b7b043553e5f7089c691900bc77fbb194456bcb
manojpodugu <psmk126@gmail.com>
2025-10-18 12:05:48 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,34 @@
+output "public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.app.public_ip
+}
+
+output "public_dns" {
+  description = "Public DNS name of the EC2 instance"
+  value       = aws_instance.app.public_dns
+}
+
+output "s3_bucket" {
+  description = "S3 bucket created for backups/static files"
+  value       = aws_s3_bucket.backups.bucket
+}
+
+output "vpc_id" {
+  description = "VPC ID created"
+  value       = aws_vpc.main.id
+}
+
+output "subnet_id" {
+  description = "Subnet ID created"
+  value       = aws_subnet.public.id
+}
+
+output "instance_profile" {
+  description = "IAM instance profile attached to EC2 (for S3 access)"
+  value       = aws_iam_instance_profile.ec2_profile.name
+}
+
+output "app_url" {
+  description = "Suggested app URL (set DNS to this IP)"
+  value       = "https://${var.app_subdomain}.${var.domain}"
+}
