ae8513d2f11fd325f2d3602ea6d63b573f2c4f93
manojpodugu <psmk126@gmail.com>
2025-10-18 13:00:57 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "app" {
-  ami                    = data.aws_ami.ubuntu.id
+  ami                    = ami-0360c520857e3138f
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
