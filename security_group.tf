f4a4a26425803ae642771605e3bca320100b58a2
manojpodugu <psmk126@gmail.com>
2025-10-18 11:59:34 +0530

    Create security_group.tf

---
+++ security_group.tf
@@ -0,0 +1,42 @@
+resource "aws_security_group" "ec2_sg" {
+  name        = "${var.project_name}-${var.environment}-sg"
+  description = "Allow SSH (restrict admin_ip_cidr), HTTP and HTTPS"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.admin_ip_cidr]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "All outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-sg"
+    Environment = var.environment
+  }
+}
