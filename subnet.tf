c5d8cd4b603fb4ed253a6b31be9145b09a684c62
manojpodugu <psmk126@gmail.com>
2025-10-18 12:10:05 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,1 +1,14 @@
 
+data "aws_availability_zones" "available" {}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-public-subnet"
+    Environment = var.environment
+  }
+}
